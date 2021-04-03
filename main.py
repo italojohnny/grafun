679ab4e4147f20e085763b06d1800e21db74ff59
italojohnny <italojohnnydosanjos@gmail.com>
2021-04-03 13:23:44 -0300

    Adiciona novo parametro para lista de caminhos

--- main.py
+++ main.py
@@ -2,13 +2,22 @@
 # coding: utf-8
 
 import argparse
+import pathlib
 
 def get_argparse():
-    return argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         prog = 'grafun',
         description = 'creates function call graph.',
         epilog = 'for more access: https://github.com/italojohnny/grafun'
     )
+    parser.add_argument(
+        nargs='*',
+        dest='paths',
+        default=[pathlib.Path().absolute()],
+        type=pathlib.Path,
+        help='list of directories and files',
+    )
+    return parser
 
 if __name__ == '__main__':
     a = get_argparse().parse_args(['-h'])

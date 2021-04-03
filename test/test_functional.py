8a8db102ad16cfebc5cd13d6a6a04198f06ea4b7
italojohnny <italojohnnydosanjos@gmail.com>
2021-04-03 10:58:28 -0300

    Cria 'teste funcional' verifica criacao de imagem

--- test/test_functional.py
+++ test/test_functional.py
@@ -3,8 +3,11 @@
 
 import unittest
 import subprocess
+import pathlib
 
 PROGRAM_NAME = 'main.py'
+EXAMPLE_FILE = 'resource/script.py'
+OUTPUT_IMAGE = 'output.png'
 
 class TestCommandLine(unittest.TestCase):
 
@@ -16,6 +19,20 @@ class TestCommandLine(unittest.TestCase):
         result = subprocess.run(call, capture_output=True)
         output = result.stdout.decode('utf-8')
         self.assertIn('help', output)
+
+
+    def test_output_image(self):
+        # Usuario chama o programa passando um arquivo com funcoes:
+        # Programa gera um arquivo de imagem png
+        path = pathlib.Path(EXAMPLE_FILE)
+        self.assertTrue(path.is_file())
+
+        call = 'python {} {}'.format(PROGRAM_NAME, EXAMPLE_FILE)
+        call = call.split()
+        result = subprocess.run(call, capture_output=True)
+
+        path = pathlib.Path(OUTPUT_IMAGE)
+        self.assertTrue(path.is_file())
 
 
 if __name__ == '__main__':
